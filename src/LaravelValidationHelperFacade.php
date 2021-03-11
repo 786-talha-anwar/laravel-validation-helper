82ea49dda1fd1c5497981b8a6f1abc54099b6776
wixpadev <experimenting49@gmail.com>
2021-03-11 16:01:27 +0500

    fixed

--- src/LaravelValidationHelperFacade.php
+++ src/LaravelValidationHelperFacade.php
@@ -3,14 +3,12 @@
 namespace TalhaAnwar\LaravelValidationHelper;
 
 use Illuminate\Support\Facades\Facade;
-use talhaanwar\LaravelValidationHelper\Traits\LVH;
 
 /**
  * @see \TalhaAnwar\LaravelValidationHelper\Skeleton\SkeletonClass
  */
 class LaravelValidationHelperFacade extends Facade
 {
-	use LVH;
     /**
      * Get the registered name of the component.
      *
