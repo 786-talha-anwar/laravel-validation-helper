03567a9848f2daacf951060c2f066223f1444d82
dev-talha-anwar <56028813+dev-talha-anwar@users.noreply.github.com>
2023-04-13 08:32:29 +0500

    Update ValidationHelperFacade.php

--- src/ValidationHelperFacade.php
+++ src/ValidationHelperFacade.php
@@ -3,12 +3,14 @@
 namespace TalhaAnwar\LaravelValidationHelper;
 
 use Illuminate\Support\Facades\Facade;
+use TalhaAnwar\LaravelValidationHelper\ValidationHelper;
 
 /**
  * @see \TalhaAnwar\LaravelValidationHelper\Skeleton\SkeletonClass
  */
 class ValidationHelperFacade extends Facade
 {
+    use ValidationHelper;
     /**
      * Get the registered name of the component.
      *
